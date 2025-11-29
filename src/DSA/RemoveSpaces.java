9ed1ffec923cfbe47385ca36a96754e229dd0624
aashishgavit <aashishgavit29@gmail.com>
2025-11-29 23:33:31 +0530

    Remove Space

---
+++ src/DSA/RemoveSpaces.java
@@ -0,0 +1,8 @@
+package DSA;
+
+public class RemoveSpaces {
+    public static void main(String[] args) {
+        String s = "J a v a  Code ";
+        System.out.println(s.replaceAll("\\s", ""));
+    }
+}
