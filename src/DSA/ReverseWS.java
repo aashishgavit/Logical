dbb9c4522cd5aa898fe849004e15f55735d0e133
aashishgavit <aashishgavit29@gmail.com>
2025-11-30 23:13:33 +0530

    Reverse (No String)

---
+++ src/DSA/ReverseWS.java
@@ -0,0 +1,14 @@
+package DSA;
+
+public class ReverseWS{
+    public static void main(String[] args) {
+        int n = 12345, rev = 0;
+
+        while (n > 0) {
+            rev = rev * 10 + n % 10;
+            n /= 10;
+        }
+
+        System.out.println("Reverse: " + rev);
+    }
+}
