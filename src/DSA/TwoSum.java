8f5242dcf97f602250f458170084923ee4291717
aashishgavit <aashishgavit29@gmail.com>
2025-12-22 13:53:35 +0530

    Find indices of two numbers that add up to a target.

---
+++ src/DSA/TwoSum.java
@@ -0,0 +1,27 @@
+package DSA;
+
+import java.util.*;
+
+public class TwoSum 
+{
+    public static int[] twoSum(int[] nums, int target) 
+    {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (map.containsKey(diff)) 
+            {
+                return new int[] { map.get(diff), i };
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] {};
+    }
+    public static void main(String[] args) 
+    {
+        int[] arr = {2,7,11,15};
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum(arr, target)));
+    }
+}
+
